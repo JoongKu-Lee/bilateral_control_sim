26909bd64fa8c7d0325933529827923a90ea62d6
JoongKu Lee <iamjoong9@kaist.ac.kr>
2021-06-09 23:52:14 +0900

    cache clear

--- build/myrobot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/joongku/ros/teleop_sim_ws/src/myrobot_description
-# Build directory: /home/joongku/ros/teleop_sim_ws/src/build/myrobot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
